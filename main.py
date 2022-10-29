f24b5394274f740ce6c463bcd528918a3810215b
crasgaitis <crasgaitis@gmail.com>
2022-10-29 16:53:31 -0700

    baseline

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import sklearn
+import pandas as pd
+import numpy as np
+import math
+from PIL import Image
+import pickle
+# import joblib
+import random
+
+DATA = 'Trees.csv'
 
