13ee393b8059634a49d7b7a101707839206fbfd3
crasgaitis <crasgaitis@gmail.com>
2022-10-29 23:37:19 -0700

    MODEL WORKS MOST OF THEM TIME OMG

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 import math
 from PIL import Image
 import sklearn
-## from pipeline import full_pipeline
 import pickle
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import Pipeline
@@ -17,7 +16,10 @@ from sklearn.preprocessing import OneHotEncoder
 with gzip.open('ml_model.pkl', 'rb') as f:
     p = pickle.Unpickler(f)
     clf = p.load()    
-    
+
+with open("pipeline.pkl", 'rb') as file2:
+    full_pipeline = pickle.load(file2)
+
 @st.cache
 def load_data(file):
     data = pd.read_csv(file)
@@ -29,9 +31,7 @@ data = data[['DIAM', 'SITETYPE', 'GENUS', 'PRIMARYDISTRICTCD']]
 
 st.title("Find your tree's new home")
 
-max_DIAM = int(data["DIAM"].max())
-min_DIAM = 0
-DIAM = st.slider("Tree Diameter (inches)", min_DIAM, max_DIAM, int((min_DIAM+max_DIAM)/2))
+DIAM = st.selectbox("Diameter (inches)", ('2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17'))
 
 SITETYPE = st.selectbox("Site type", ("STRP", "PIT"))
 
@@ -42,23 +42,19 @@ submit = st.button("Calculate")
 user_input = pd.DataFrame(np.array([[DIAM, SITETYPE, GENUS]]), columns =['DIAM', 'SITETYPE', 'GENUS'])
 st.write(user_input)
 
-num = ['DIAM']
-cat = ['SITETYPE', 'GENUS']
+input_ = {
+    "DIAM": int(DIAM),
+    "SITETYPE": [SITETYPE],
+    "GENUS": [GENUS]
+}
+user_input = pd.DataFrame(input_) 
+user_input
 
-num_pipeline = Pipeline([
-  ("std_scaler", StandardScaler())
-])
-
-col_pipeline = ColumnTransformer([  
-  ("num", num_pipeline, num), 
-  ("cat", OneHotEncoder(), cat),  
-])
-
-full_pipeline = Pipeline([   
-  ("col_pipeline", col_pipeline)
-])
-
+cat = ['DIAM', 'SITETYPE', 'GENUS']
 
 if submit:
     user_input_prep = full_pipeline.transform(user_input)
     st.write(user_input_prep)
+    
+    pred = clf.predict(user_input_prep)
+    st.write(pred)
