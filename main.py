cd6fcf12fa91a0c387291f4f26088879e6be342e
crasgaitis <crasgaitis@gmail.com>
2022-10-29 17:39:57 -0700

    yay

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import streamlit as st
-import sklearn
 import pandas as pd
 import numpy as np
 import math
 from PIL import Image
+import sklearn
+## from pipeline import full_pipeline
 import pickle
-# import joblib
-import random
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+import gzip
 
-DATA = 'Trees.csv'
-
+with gzip.open('ml_model.pkl', 'rb') as f:
+    p = pickle.Unpickler(f)
+    clf = p.load()    
+    
 @st.cache
 def load_data(file):
     data = pd.read_csv(file)
-    lowercase = lambda x: str(x).lower()
-    data.rename(lowercase, axis='columns', inplace=True)
     return data
+
+DATA = 'Trees.csv'
+data = load_data(DATA)
+
+st.title("Find your tree's new home")
+
+max_DIAM = int(data["DIAM"].max())
+min_DIAM = 0
+DIAM = st.slider("Tree Diameter (inches)", min_DIAM, max_DIAM, int((min_DIAM+max_DIAM)/2))
+
+SITETYPE = st.selectbox("Site type", ("STRP", "PIT"))
+
+GENUS = st.selectbox("Genus", ("Acer", "Prunus", "Malus", "Crataegus", "Quercus", "Cornus", "Pyrus"))
+
+submit = st.button("Calculate")
+
+if submit:
+    user_input_prepared = pd.DataFrame(np.array([[DIAM, SITETYPE, GENUS]]), columns =['DIAM', 'SITETYPE', 'GENUS'])
+    user_input_prepared = full_pipeline.transform(user_input_prepared)
+
 
